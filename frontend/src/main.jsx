5bc54c342394e3f01ca05d603666f8fc60787970
Reymart <reymartomega18@gmail.com>
2025-11-18 00:39:35 +0800

    update again

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,47 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
 
-createRoot(document.getElementById('root')).render(
+/* -----------------------------
+   Remove Tracking URL Parameters
+   (fbclid, gclid, utm_*, msclkid, etc.)
+--------------------------------*/
+const trackingParams = [
+  "fbclid",
+  "gclid",
+  "msclkid",
+  "utm_source",
+  "utm_medium",
+  "utm_campaign",
+  "utm_term",
+  "utm_content",
+  "utm_id",
+];
+
+const params = new URLSearchParams(window.location.search);
+let changed = false;
+
+trackingParams.forEach((param) => {
+  if (params.has(param)) {
+    params.delete(param);
+    changed = true;
+  }
+});
+
+// Only update URL if changes were made
+if (changed) {
+  const newQuery = params.toString();
+  const cleanUrl =
+    window.location.pathname + (newQuery ? `?${newQuery}` : "");
+  window.history.replaceState({}, "", cleanUrl);
+}
+
+/* -----------------------------
+   React App Mount
+--------------------------------*/
+createRoot(document.getElementById("root")).render(
   <StrictMode>
     <App />
-  </StrictMode>,
-)
+  </StrictMode>
+);
